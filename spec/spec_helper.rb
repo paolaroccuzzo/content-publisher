9ae6a0edbdf74e9a8329e8c04133de5651fa48ea
Emma Beynon <emma.beynon@digital.cabinet-office.gov.uk>
2018-07-10 12:26:10 +0100

    Run Simplecov to report on test coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,9 @@ require "pry"
 require "byebug"
 require "webmock"
 require "timecop"
+require "simplecov"
+
+SimpleCov.start
 
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
