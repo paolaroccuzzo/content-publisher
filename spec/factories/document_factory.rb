b7b13243ff9bbb61c32c458e3bcbefbc6266c6c1
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2018-08-02 15:25:49 +0100

    Specifically say in_schema for document factory traits

--- spec/factories/document_factory.rb
+++ spec/factories/document_factory.rb
@@ -7,13 +7,13 @@ FactoryBot.define do
     base_path { "/#{SecureRandom.alphanumeric(8)}" }
     document_type { DocumentTypeSchema.all.reject(&:managed_elsewhere).sample.id }
 
-    trait :with_body do
+    trait :with_body_in_schema do
       document_type do
         DocumentTypeSchema.all.select { |schema| schema.contents.any? { |field| field.id == "body" } }.sample.id
       end
     end
 
-    trait :with_associations do
+    trait :with_associations_in_schema do
       document_type do
         DocumentTypeSchema.all.select { |schema| schema.associations.any? }.sample.id
       end
