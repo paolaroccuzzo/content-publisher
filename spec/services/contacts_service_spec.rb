4d7f164c7e2d2b8f6438d1378eaf14dd2e603d8e
Kevin Dew <kevindew@me.com>
2018-11-13 12:45:55 +0000

    Load all contacts from Publishing API into Content Publisher
    This allows us to present to users a list of all the contacts that are
    available - and relies on Publishing API having the links part of the
    GET editions endpoint: https://github.com/alphagov/publishing-api/pull/1383
    
    It's a bit of a brute to load in all the contacts - at requests of 1000
    a time it takes 4 HTTP requests, these seem to be pretty fast requests
    but still on my VM I can hit 2.5 seconds for these so they aren't ideal.
    
    I had some challenges with hashes indexed with symbols or with strings
    (as the JSON responses return strings) and decided to stick with
    everything being strings. If this feels inconsistent we can probably use
    HashWithIndifferentAccess instead.

--- spec/services/contacts_service_spec.rb
+++ spec/services/contacts_service_spec.rb
@@ -5,40 +5,62 @@ RSpec.describe ContactsService do
     context "when a contact is found" do
       it "returns the contact" do
         content_id = SecureRandom.uuid
-        contact = {
-          "content_id" => content_id,
-          "locale" => "en",
-          "title" => "Clark Kent",
-          "schema_name" => "contact",
-          "document_type" => "contact",
-        }
-        publishing_api_has_item(contact)
+        editions = [ { "content_id" => content_id, "locale" => "en", "title" => "Clark Kent" } ]
+        publishing_api_get_editions(editions, ContactsService::EDITION_PARAMS)
 
-        expect(ContactsService.new.by_content_id(content_id)).to eq(contact)
+        expect(ContactsService.new.by_content_id(content_id)).to eq(editions.first)
       end
     end
 
     context "when a contact is not found" do
       it "returns nil" do
-        content_id = SecureRandom.uuid
-        publishing_api_does_not_have_item(content_id)
-        expect(ContactsService.new.by_content_id(content_id)).to be_nil
+        publishing_api_get_editions([], ContactsService::EDITION_PARAMS)
+        expect(ContactsService.new.by_content_id(SecureRandom.uuid)).to be_nil
+      end
+    end
+  end
+
+  describe "#all_by_organisation" do
+    context "when there are organisations with contacts" do
+      it "returns each organisation with their contacts" do
+        org_1 = { "content_id" => SecureRandom.uuid, "internal_name" => "Org 1" }
+        org_2 = { "content_id" => SecureRandom.uuid, "internal_name" => "Org 2" }
+        publishing_api_has_linkables([org_1, org_2], document_type: "organisation")
+
+        org_1_contacts = [
+          { "content_id" => SecureRandom.uuid, "locale" => "en", "title" => "Contact 1", "links" => { "organisations" => [org_1["content_id"]] } },
+          { "content_id" => SecureRandom.uuid, "locale" => "en", "title" => "Contact 2", "links" => { "organisations" => [org_1["content_id"]] } },
+        ]
+        org_2_contacts = [
+          { "content_id" => SecureRandom.uuid, "locale" => "en", "title" => "Contact 3", "links" => { "organisations" => [org_2["content_id"]] } },
+        ]
+        publishing_api_get_editions(org_1_contacts + org_2_contacts, ContactsService::EDITION_PARAMS)
+
+        expect(ContactsService.new.all_by_organisation).to match([
+          { "name" => org_1["internal_name"], "content_id" => org_1["content_id"], "contacts" => org_1_contacts },
+          { "name" => org_2["internal_name"], "content_id" => org_2["content_id"], "contacts" => org_2_contacts },
+        ])
       end
     end
 
-    context "when the returned result is not a contact" do
-      it "returns nil" do
-        content_id = SecureRandom.uuid
-        news = {
-          content_id: content_id,
-          locale: "en",
-          title: "Breaking news",
-          schema_name: "news_article",
-          document_type: "news_story",
-        }
-        publishing_api_has_item(news)
+    context "when there are organisations without contacts" do
+      it "returns them despite their lack of contacts" do
+        org_1 = { "content_id" => SecureRandom.uuid, "internal_name" => "Org 1" }
+        publishing_api_has_linkables([org_1], document_type: "organisation")
 
-        expect(ContactsService.new.by_content_id(content_id)).to be_nil
+        publishing_api_get_editions([], ContactsService::EDITION_PARAMS)
+
+        expect(ContactsService.new.all_by_organisation).to match([
+          { "name" => org_1["internal_name"], "content_id" => org_1["content_id"], "contacts" => [] },
+        ])
+      end
+    end
+
+    context "when there are no organisations" do
+      it "returns an empty array" do
+        publishing_api_has_linkables([], document_type: "organisation")
+        publishing_api_get_editions([], ContactsService::EDITION_PARAMS)
+        expect(ContactsService.new.all_by_organisation).to eql([])
       end
     end
   end
